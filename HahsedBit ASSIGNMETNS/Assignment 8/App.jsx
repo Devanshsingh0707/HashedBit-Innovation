c082cac4bab6154af52ff9058d897b9de67ca929
Devansh Singh <dev.devansh.singh007@gmail.com>
2026-01-21 17:53:31 +0530

    Add files via upload

---
+++ HahsedBit ASSIGNMETNS/Assignment 8/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/FreSauce/json-ipl/data")
+      .then((res) => res.json())
+      .then((data) => {
+        // sort by NRR (ascending)
+        const sortedData = data.sort((a, b) => a.NRR - b.NRR);
+        setTeams(sortedData);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>IPL Season 2022 Points</h1>
+
+      <div className="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Team</th>
+              <th>Matches</th>
+              <th>Won</th>
+              <th>Lost</th>
+              <th>Tied</th>
+              <th>NRR</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            {teams.map((team, index) => (
+              <tr key={team.id}>
+                <td>{index + 1}</td>
+                <td>{team.Team}</td>
+                <td>{team.Matches}</td>
+                <td>{team.Won}</td>
+                <td>{team.Lost}</td>
+                <td>{team.Tied}</td>
+                <td>{team.NRR}</td>
+                <td>{team.Points}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
